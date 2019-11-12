f98e1de5eb04f8c2c3d45b3e3e4853421c9335cf
zigrable <atu8815@gmail.com>
2019-11-13 00:51:26 +0900

    auto-reload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,8 @@ $(function(){
 
   function buildMessage(message){
     var image =  message.image ? `<img class="lower-message__image" src= ${message.image}`: '';
-    var html = `<div class="upper-message">
+    var html = `<div class="message"  data-id=${message.id } >
+                 <div class="upper-message">
                   <div class="upper-message__user-name">
                   ${message.user_name}
                 </div>
@@ -19,6 +20,7 @@ $(function(){
                   `
     return html;
   }
+
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this)
@@ -43,6 +45,32 @@ $(function(){
       $('.form__submit').prop('disabled', false);
     })
   })
-});
 
+
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data('id');
+        $.ajax({
+          url: 'api/messages',
+          type: 'GET',
+          dataType: 'json',
+          data: {id: last_message_id}
+        })
+        .done(function(messages) {
+          console.log(messages)
+            var insertHTML = '';
+            messages.forEach(function (message) {
+              insertHTML = buildMessage(message); 
+              $('.contents__text').append(insertHTML);
+            })
+              $('.contents__text').animate({scrollTop: $('.contents__text')[0].scrollHeight}, 0);
+        })
+        .fail(function() {
+          console.log(last_message_id);
+        });
+      }
+    };
+  setInterval(reloadMessages, 5000);
+
+});
 
