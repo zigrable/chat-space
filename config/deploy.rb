3e2ac1d48e30628148112b3414ee06b845fd3e52
zigrable <atu8815@gmail.com>
2019-11-13 19:38:06 +0900

    set

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,12 +33,15 @@ namespace :deploy do
   end
 end
 
+
+
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
+
 set :linked_files, %w{ config/secrets.yml }
 
 # 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
