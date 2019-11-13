1749335ec93089982f3c557ce50ac5aba25aace3
zigrable <atu8815@gmail.com>
2019-11-13 20:29:06 +0900

    time

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,9 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
-      config.i18n.default_locale = :ja
     end
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
